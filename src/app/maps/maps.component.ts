6ecc46352648b8f87480dd6fb328b64c6c875699
Gabriel Santana <95727078+gapesasi@users.noreply.github.com>
2024-08-02 18:52:57 -0300

    feat: add info window

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { GoogleMap, MapMarker } from '@angular/google-maps';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'map',
   templateUrl: './maps.component.html',
   styleUrl: './maps.component.css',
   standalone: true,
-  imports: [GoogleMap, MapMarker],
+  imports: [GoogleMap, MapMarker, MapInfoWindow],
 })
 export class MapComponent {
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
   hpsLat = -18.970594;
   hpsLng = -49.458939;
   center: google.maps.LatLngLiteral = { lat: this.hpsLat, lng: this.hpsLng };
@@ -17,6 +18,7 @@ export class MapComponent {
   markerPositions: google.maps.LatLngLiteral[] = [
     { lat: this.hpsLat, lng: this.hpsLng },
   ];
+  options: google.maps.InfoWindowOptions = {};
 
   moveMap(event: google.maps.MapMouseEvent) {
     this.center = event.latLng!.toJSON();
@@ -24,5 +26,22 @@ export class MapComponent {
 
   move(event: google.maps.MapMouseEvent) {
     this.display = event.latLng!.toJSON();
+  }
+
+  openInfoWindow(marker: MapMarker) {
+    let markerLat = marker.getPosition()?.lat();
+    let markerLng = marker.getPosition()?.lng();
+    let opts = {
+      headerContent: `HPS Tecnologia`,
+      content: `Latitude: ${markerLat}<br />Longitude: ${markerLng}`,
+    };
+    let cent = {
+      lat: markerLat!,
+      lng: markerLng!,
+    };
+
+    this.options = opts;
+    this.center = cent;
+    this.infoWindow!.open(marker);
   }
 }
